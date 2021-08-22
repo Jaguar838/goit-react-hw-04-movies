cb51daac5483b71b8607b82b55d4739af6ec5206
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-22 09:10:54 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { NotFoundView } from 'views/NotFoundView';
+import LoaderUI from 'UI/LoaderUI/LoaderUI';
+// import { NotFoundView } from 'views/NotFoundView';
 // import { ROUTE_PATHS } from 'services/route-paths';
-
+const HomeView = lazy(() =>
+  import('views/HomePage/HomePage' /* webpackChunkName: "home-view" */),
+);
+const MoviesView = lazy(() =>
+  import('views/MoviesView.js' /* webpackChunkName: "movies-view" */),
+);
+const NotFoundView = lazy(() =>
+  import(
+    'views/NotFoundView/NotFoundView' /* webpackChunkName: "not-found-view" */
+  ),
+);
+const MovieDetailsView = lazy(() =>
+  import(
+    'views/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movie-details-view" */
+  ),
+);
 const App = () => {
   return (
     <>
-      <Switch>
-        {/* <Route path={ROUTE_PATHS._()} exact component={ContactsView} /> */}
-        <Route component={NotFoundView} />
-      </Switch>
+      <Suspense fallback={<LoaderUI />}>
+        <Switch>
+          {/* <Route path={ROUTE_PATHS._()} exact component={ContactsView} /> */}
+          <Route component={NotFoundView} />
+        </Switch>
+      </Suspense>
     </>
   );
 };
