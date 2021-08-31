4ea4d7b2152f1463a06c4cd742c56f1a79913a24
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-31 15:54:44 +0300

    Update Navigations.js

--- src/components/Navigation/Navigations.js
+++ src/components/Navigation/Navigations.js
@@ -1,6 +1,4 @@
 import { NavLink } from 'react-router-dom';
-// import { ImHome } from 'react-icons/im';
-// import { IoSearchCircleSharp } from 'react-icons/io5';
 import css from './Navigations.module.css';
 
 export default function Navigations() {
@@ -14,14 +12,12 @@ export default function Navigations() {
       >
         <span className={css.search}>Home</span>
 
-        {/*   <ImHome /> */}
       </NavLink>
       <NavLink
         className={css.link}
         activeClassName={css.activeLink}
         to="/movies"
       >
-        {/* <IoSearchCircleSharp size="25" /> */}
         <span className={css.search}>SearchMovie</span>
       </NavLink>
     </nav>
